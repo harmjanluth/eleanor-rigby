8190dba69187955db3e69edd2faa7622a0fc7a28
Harm Jan Luth <harmjan.luth@marviq.com>
2015-01-14 12:18:44 +0100

    Test mongoose / mongolab connection

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+var mongoose = require ("mongoose");
+var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/tmp';
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
   response.send('[eleanor-rigby:tst]');
+});
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
 });
 
 app.listen(app.get('port'), function() {
